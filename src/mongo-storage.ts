c3c20facfab88c42e316fc2e3a61c714349bceab
Zainan Victor Zhou <zzn@google.com>
2019-03-11 22:20:25 -0700

    add: add to mongodb

---
+++ src/mongo-storage.ts
@@ -0,0 +1,35 @@
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require('mongodb').ObjectID;
+const log4js = require('log4js');
+const logger = log4js.getLogger();
+
+
+export class MongoStorage {
+  private mongodbClient;
+  private mongodbDb;
+
+  public async init() {
+    const MongoClient = require('mongodb').MongoClient;
+    const dbName = process.env.MONGODB_URI.split(`/`)[-1];
+    // Use connect method to connect to the server
+    this.mongodbClient = await MongoClient.connect(process.env.MONGODB_URI);
+    this.mongodbDb = this.mongodbClient.db(dbName);
+  }
+
+  public async close() {
+    await this.mongodbClient.close();
+  }
+
+  /**
+   *
+   * @param collectionName
+   * @param obj
+   * @param numOfMonth
+   *
+   * @returns Promise of txId
+   */
+  public async save(collectionName, obj) {
+    let collection = this.mongodbDb.collection(collectionName);
+    await collection.insertOne(obj);
+  }
+}
