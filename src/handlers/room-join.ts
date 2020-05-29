3a5833aa4705c0d7503d06978878815abaa7694f
William Chen <kis87988@gmail.com>
2020-05-28 23:06:53 -0700

    refactor: create logger, handlers, and util

---
+++ src/handlers/room-join.ts
@@ -0,0 +1,26 @@
+import { logger } from "../logger";
+import { Handler, WechatyRoom, WechatyContact } from "./handler";
+
+export class RoomJoin extends Handler {
+  public async listener(
+    room: WechatyRoom,
+    inviteeList: WechatyContact[],
+    inviter: WechatyContact,
+    date: Date
+  ) {
+    try {
+      const roomTopic = await room.topic();
+      const inviteeNameList = inviteeList.map((c) => c.toString()).join(",");
+
+      // if bot-self join the new room
+      if (inviteeList[0].self()) {
+        logger.info(`Bot join ${roomTopic} invite by ${inviter}`);
+        return;
+      }
+
+      logger.info(`${inviteeNameList} join ${roomTopic} invite by ${inviter}`);
+    } catch (e) {
+      logger.error(`room-join event exception: ${e.stack}`);
+    }
+  }
+}
--- src/mongo-storage.ts
+++ src/mongo-storage.ts
@@ -1,15 +1,14 @@
-const MongoClient = require('mongodb').MongoClient;
-const ObjectId = require('mongodb').ObjectID;
-const log4js = require('log4js');
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const log4js = require("log4js");
 const logger = log4js.getLogger();
-
 
 export class MongoStorage {
   private mongodbClient;
   private mongodbDb;
 
   public async init() {
-    const MongoClient = require('mongodb').MongoClient;
+    const MongoClient = require("mongodb").MongoClient;
     const dbName = process.env.MONGODB_URI.split(`/`)[-1];
     // Use connect method to connect to the server
     this.mongodbClient = await MongoClient.connect(process.env.MONGODB_URI);
