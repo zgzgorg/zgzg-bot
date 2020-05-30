d9a71a37ac33650953f2f1362f82ae920122e21b
William Chen <kis87988@gmail.com>
2020-05-30 14:50:57 -0700

    add: get room list related function

--- src/handlers/handler.ts
+++ src/handlers/handler.ts
@@ -1,7 +1,8 @@
+import { logger } from "../logger";
 import { Wechaty } from "wechaty";
 import { MongoStorage } from "../mongo-storage";
 import * as util from "../util";
-export {
+import {
   Contact as WechatyContact,
   ContactSelf as WechatyContactSelf,
   Message as WechatyMessage,
@@ -10,7 +11,17 @@ export {
   RoomInvitation as WechatyRoomInvitation,
 } from "wechaty/dist/src/user/";
 
+export {
+  WechatyContact,
+  WechatyContactSelf,
+  WechatyMessage,
+  WechatyRoom,
+  WechatyFriendship,
+  WechatyRoomInvitation,
+};
+
 export abstract class Handler {
+  // TODO: add Wechaty cache for findAll() room
   protected bot: Wechaty;
   protected mongoStorage: MongoStorage;
   constructor(bot: Wechaty, mongoStorage: MongoStorage) {
@@ -26,6 +37,42 @@ export abstract class Handler {
 
   protected get_random_int_inclusive(min: number, max: number) {
     return util.get_random_int_inclusive(min, max);
+  }
+
+  protected async getRoomListToString(roomList: WechatyRoom[]) {
+    let roomItemList: string = "rooms list:\n";
+
+    try {
+      for (let i = 0; i < roomList.length; i++) {
+        const roomTopic = await roomList[i].topic();
+        roomItemList += i + 1 + `. ${roomTopic} id: ${roomList[i].id}\n`;
+      }
+
+      return roomItemList;
+    } catch (err) {
+      logger.error("Bot", "getRoomList() exception: " + err.stack);
+      return "Can not get rooms list";
+    }
+  }
+
+  protected async getRoomList(regexpGroup: RegExp[] = []) {
+    try {
+      const roomList: WechatyRoom[] = await this.bot.Room.findAll();
+
+      // means get all rooms
+      if (regexpGroup.length == 0) return roomList;
+
+      const filterRoomList: WechatyRoom[] = roomList.filter(async (room) => {
+        const roomTopic = await room.topic();
+        for (const regexp of regexpGroup)
+          if (regexp.test(roomTopic)) return true;
+        return false;
+      });
+      return filterRoomList;
+    } catch (error) {
+      logger.error("Bot", "getRoomList() exception: " + error.stack);
+      return [];
+    }
   }
 
   public abstract listener(...args: any[]): void;
