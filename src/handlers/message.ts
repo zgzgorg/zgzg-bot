76ac96bb68348e7799e5c52ce00da5f1aa1078e2
William Chen <kis87988@gmail.com>
2020-05-30 17:15:57 -0700

    add: try-catch block

--- src/handlers/message.ts
+++ src/handlers/message.ts
@@ -23,52 +23,56 @@ export class Message extends Handler {
     logger.debug({ event: "message", data: message });
     this.mongoStorage.save(`WeChatyPadproMessage`, message);
 
-    const fromContact: WechatyContact | null = message.from();
-    const messageText: string = message.text();
-    const messageTextTw: string = cn2tw(messageText);
-    const fromRoom: WechatyRoom | null = message.room();
-    const messageType: WechatyMessageType = message.type();
-    const isMiniProgram: boolean = this.isMiniProgram(message);
-    let replyText: string = "";
+    try {
+      const fromContact: WechatyContact | null = message.from();
+      const messageText: string = message.text();
+      const messageTextTw: string = cn2tw(messageText);
+      const fromRoom: WechatyRoom | null = message.room();
+      const messageType: WechatyMessageType = message.type();
+      const isMiniProgram: boolean = this.isMiniProgram(message);
+      let replyText: string = "";
 
-    if (
-      message.self() ||
-      !fromContact ||
-      fromContact.type() == WechatyContact.Type.Official
-    ) {
-      // TODO: we can check bot is alive or not here
-      return;
-    }
-
-    // TODO: Check blocklist here
-
-    if (fromRoom) {
-      // TODO: message from group chat room
-      return;
-    }
-
-    // User talk to bot directly
-    const roomList2020s: WechatyRoom[] = await this.getRoomList2020s();
-    let room: WechatyRoom | null = null;
-    if ((room = await this.roomMatch(messageText, roomList2020s))) {
-      try {
-        await this.addUserToRoom(fromContact, room);
-        const roomTopic = await room.topic();
-        const roomAnnounce: string = await room.announce().catch((e) => "");
-        replyText = `歡迎加入 ${roomTopic},請看以下群公告:\n` + roomAnnounce;
-      } catch (error) {
-        replyText = error.message + `\n威廉　wechat id: kis87988`;
+      if (
+        message.self() ||
+        !fromContact ||
+        fromContact.type() == WechatyContact.Type.Official
+      ) {
+        // TODO: we can check bot is alive or not here
+        return;
       }
-    } else {
-      const roomList2020sContent = await this.getRoomListToString(
-        roomList2020s
-      );
-      replyText =
-        `不好意思，請告訴我你想加的群，以下是可以加入的群列表\n` +
-        roomList2020sContent +
-        `\n請輸入編號或群名加群，謝謝`;
+
+      // TODO: Check blocklist here
+
+      if (fromRoom) {
+        // TODO: message from group chat room
+        return;
+      }
+
+      // User talk to bot directly
+      const roomList2020s: WechatyRoom[] = await this.getRoomList2020s();
+      let room: WechatyRoom | null = null;
+      if ((room = await this.roomMatch(messageText, roomList2020s))) {
+        try {
+          await this.addUserToRoom(fromContact, room);
+          const roomTopic = await room.topic();
+          const roomAnnounce: string = await room.announce().catch((e) => "");
+          replyText = `歡迎加入 ${roomTopic},請看以下群公告:\n` + roomAnnounce;
+        } catch (error) {
+          replyText = error.message + `\n威廉　wechat id: kis87988`;
+        }
+      } else {
+        const roomList2020sContent = await this.getRoomListToString(
+          roomList2020s
+        );
+        replyText =
+          `不好意思，請告訴我你想加的群，以下是可以加入的群列表\n` +
+          roomList2020sContent +
+          `\n請輸入編號或群名加群，謝謝`;
+      }
+      if (this.isTwText(messageText)) replyText = cn2tw(replyText);
+      await message.say(replyText);
+    } catch (e) {
+      logger.error("Bot", "message event exception: %s", e.stack);
     }
-    if (this.isTwText(messageText)) replyText = cn2tw(replyText);
-    await message.say(replyText);
   }
 }
