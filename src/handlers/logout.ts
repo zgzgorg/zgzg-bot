7b37225b68b938812793c11c4c4388f38cdf9f31
William Chen <kis87988@gmail.com>
2020-05-30 21:49:55 -0700

    add: reason parameter

--- src/handlers/logout.ts
+++ src/handlers/logout.ts
@@ -2,7 +2,7 @@ import { logger } from "../logger";
 import { Handler, WechatyContactSelf } from "./handler";
 
 export class Logout extends Handler {
-  public listener(botOwnerUser: WechatyContactSelf) {
-    logger.info(`bot ${botOwnerUser.name()} logout`);
+  public listener(botOwnerUser: WechatyContactSelf, reason: string) {
+    logger.info(`bot ${botOwnerUser.name()} logout, reason: ${reason}`);
   }
 }
