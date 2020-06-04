2c3b6a4cae173c3951eda6c86bfcb81ec4bad0f8
William Chen <kis87988@gmail.com>
2020-06-03 23:18:32 -0700

    add: commit message checker script for CI

---
+++ scripts/commit_range.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit
+#set -o nounset
+
+#For debugging purposes
+# set -o xtrace
+# set -o pipefail
+
+git log  `git rev-list --max-parents=0 HEAD` `git rev-parse HEAD` --pretty=%B | ./node_modules/.bin/commitlint
