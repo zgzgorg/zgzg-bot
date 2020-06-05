89054b3748b5dcb555aeacf5ddb9d5804337289b
William Chen <kis87988@gmail.com>
2020-06-04 23:16:21 -0700

    fix: bash style

--- scripts/commit_range.sh
+++ scripts/commit_range.sh
@@ -7,4 +7,4 @@ set -o errexit
 # set -o xtrace
 # set -o pipefail
 
-git log  `git rev-list --max-parents=0 HEAD` `git rev-parse HEAD` --pretty=%B | ./node_modules/.bin/commitlint
+git log "$(git rev-list --max-parents=0 HEAD)" "$(git rev-parse HEAD)" --pretty=%B | ./node_modules/.bin/commitlint
