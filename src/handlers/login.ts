87ca65d7c14ec2e1f8b66a6f1dbf077fe1a9fbd8
William Chen <kis87988@gmail.com>
2020-06-07 15:26:35 -0700

    fix: use logger instead console.log

--- src/handlers/login.ts
+++ src/handlers/login.ts
@@ -3,7 +3,7 @@ import { Handler, WechatyContactSelf } from "./handler";
 
 export class Login extends Handler {
   public listener(botOwnerUser: WechatyContactSelf) {
-    console.log(`bot ${botOwnerUser.name()} login`);
+    logger.info(`bot ${botOwnerUser.name()} login`);
     logger.info({ event: "login", data: botOwnerUser });
   }
 }
