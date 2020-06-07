9684609fd1b3fb0e5245f7301fdb602429a06f9f
William Chen <kis87988@gmail.com>
2020-06-07 15:29:38 -0700

    del: unsed layout function for GCP

--- src/logger.ts
+++ src/logger.ts
@@ -3,22 +3,9 @@ import { configure, getLogger, addLayout } from "log4js";
 const logger = getLogger();
 
 // TODO(xinbenlv): contribute to log4js main
-addLayout("asis", function (config) {
-  return function (logEvent): any {
-    return logEvent;
-  };
-});
 
 configure({
   appenders: {
-    // stackdriver: {
-    //   type: 'log4js-stackdriver-appender',
-    //   credentials: {
-    //     projectId: process.env.GCS_PROJECT_ID,
-    //     keyFilename: '.credentials/gcs.json'
-    //   },
-    //   layout: { type: 'asis' },
-    // },
     out: { type: "stdout", layout: { type: "colored" } },
   },
   categories: {
